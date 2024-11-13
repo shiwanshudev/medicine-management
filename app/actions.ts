7a500f335bc07333d5043929f08f7021c059fe98
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-11-13 21:59:52 +0530

    Add delete and add medicines server actions

--- app/actions.ts
+++ app/actions.ts
@@ -1,22 +1,74 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const searchQuery = async (formData: FormData) => {
   const searchTerm = formData.get("search-term");
 };
 
 export const updateMed = async (taken: string, id: string) => {
-  const updatedMed = await fetch(`http://localhost:3000/api/medicines/${id}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      taken: taken === "yes" ? true : false,
-    }),
-  });
+  try {
+    const response = await fetch(`${process.env.URL}/api/medicines/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        taken: taken === "yes" ? true : false,
+      }),
+    });
+    if (!response.ok)
+      throw new Error(
+        `Failed to update the medicine. Response Status: ${response.status}`
+      );
+    revalidatePath("/");
+  } catch (error) {
+    console.log(error, "Error updating the medicine.");
+  }
+};
 
-  console.log("UPDATED", updateMed);
-  revalidatePath("/");
+export const addMedicine = async (formData: FormData) => {
+  const medicineName = formData.get("medicine-name");
+  const notes = formData.get("notes");
+  try {
+    const response = await fetch(`${process.env.URL}/api/submit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        medicineName,
+        notes,
+      }),
+    });
+    if (!response.ok)
+      throw new Error(
+        `Failed to add medicine. Response Status: ${response.status}`
+      );
+
+    revalidatePath("/");
+  } catch (error) {
+    console.log(error, "Error adding medicine.");
+  } finally {
+    redirect("/");
+  }
+};
+
+export const deleteMed = async (id: string) => {
+  try {
+    const response = await fetch(`${process.env.URL}/api/medicines/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok)
+      throw new Error(
+        `Failed to delete the medicine. Response Status: ${response.status}`
+      );
+    revalidatePath("/");
+  } catch (error) {
+    console.log(error, "Error deleting the medicine.");
+  }
 };
