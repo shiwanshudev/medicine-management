b5371e4c41a28ee4f5219ac31c9d5f9103815bce
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-11-13 19:50:23 +0530

    Update nextjs and use server actions

---
+++ app/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export const searchQuery = async (formData: FormData) => {
+  const searchTerm = formData.get("search-term");
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,7 +6,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Input } from "@/components/ui/input";
+
 import {
   Select,
   SelectContent,
@@ -29,73 +27,13 @@ type FetchedMedsType = {
   taken: boolean;
 }[];
 
-export default function Home() {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [fetchedMedicines, setFetchedMedicines] = useState<FetchedMedsType>([]);
-  const [loader, setLoader] = useState(false);
+export default async function Home() {
+  const res = await fetch(`http://localhost:3000/api/medicines`);
+  const medicines = await res.json();
 
-  // Local API Calls
-  const fetchMedicines = async () => {
-    setLoader(true);
-    const response = await fetch("/api/medicines", {
-      method: "GET",
-      cache: "no-store",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    setFetchedMedicines(data);
-    setLoader(false);
-  };
-
-  const updateMedicineTaken = async (value: string, id: string) => {
-    const response = await fetch(`/api/medicines/${id}`, {
-      method: "POST",
-      cache: "no-store",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ taken: value }),
-    });
-    const data = await response.json();
-    console.log(data);
-    fetchMedicines();
-  };
-
-  useEffect(() => {
-    fetchMedicines();
-  }, []);
-
-  const handleMedicineTaken = (value: string, id: string) => {
-    updateMedicineTaken(value, id);
-  };
-  const handleDelete = async (id: string) => {
-    const response = await fetch(`/api/medicines/${id}`, {
-      method: "DELETE",
-      cache: "no-store",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    fetchMedicines();
-  };
   return (
     <div className="container mx-auto lg:px-0 px-2 ">
-      <Input
-        placeholder="Search medicines..."
-        onChange={(e) => setSearchTerm(e.target.value)}
-        value={searchTerm}
-        className="mt-5 mb-10 lg:block mx-auto"
-      />
-      {loader && (
-        <div className="text-center top-2/4 left-2/4 fixed h-screen w-full">
-          <AiOutlineLoading3Quarters className="animate-spin text-2xl block" />
-        </div>
-      )}
+      {/* <SearchMedicines /> */}
       <Table>
         {/* <TableCaption>A list of your recent medicines.</TableCaption> */}
         <TableHeader>
@@ -108,22 +46,17 @@ export default function Home() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {fetchedMedicines
-            .filter((medicine) =>
-              medicine.medicineName
-                .toLowerCase()
-                .includes(searchTerm.toLowerCase())
-            )
-            .map((medicine) => (
-              <TableRow key={medicine._id}>
-                <TableCell>
-                  {format(new Date(medicine.createdAt), "dd MMM yyyy")}
-                </TableCell>
-                <TableCell className="font-medium">
-                  {medicine.medicineName}
-                </TableCell>
-                <TableCell>
-                  {
+          {medicines.map((medicine) => (
+            <TableRow key={medicine._id}>
+              <TableCell>
+                {format(new Date(medicine.createdAt), "dd MMM yyyy")}
+              </TableCell>
+              <TableCell className="font-medium">
+                {medicine.medicineName}
+              </TableCell>
+              <TableCell>
+                {/* {
+                  <form action={formAction}>
                     <Select
                       defaultValue={medicine.taken ? "yes" : "no"}
                       onValueChange={(e) =>
@@ -138,19 +71,20 @@ export default function Home() {
                         <SelectItem value="no">No</SelectItem>
                       </SelectContent>
                     </Select>
-                  }
-                </TableCell>
-                <TableCell>{medicine.notes}</TableCell>
-                <TableCell>
-                  <Button
-                    variant={"destructive"}
-                    onClick={() => handleDelete(medicine._id)}
-                  >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
+                  </form>
+                } */}
+              </TableCell>
+              <TableCell>{medicine.notes}</TableCell>
+              <TableCell>
+                {/* <Button
+                  variant={"destructive"}
+                  onClick={() => handleDelete(medicine._id)}
+                >
+                  Delete
+                </Button> */}
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
